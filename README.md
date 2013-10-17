93fa4d8889a36a29a3e77c6e09e3c0ddfcf401c3
Hoon H <drawtree@me.com>
2013-10-17 21:15:47 +0900

    .

--- README.md
+++ README.md
@@ -17,26 +17,26 @@ This is a collection of initial configuration script.
         curl https://raw.github.com/eonil/bootstrap/master/objc2-fb9.sh | tcsh
 
   - libobjc2 1.7
-  - gnustep-base 1.24.5
+  - gnustep-base 1.24.5 (r37245, for NSFileHandle bug fix)
   - clang 3.3.
-  
-  This contains these features.
+
+  This contains these important features.
   
   - C-blocks
+  - GCD
   - ARC
   - Declared properties
-  - GCD
+  -	Objective-C literals
   
-  This script lacks support for these features.
+  This script installs gnustep-base only. No support for GUI or existing Web framework.
+  Because I don't need them now.
 
-  - GNUstep GUI
-  - GNUstep SSL/TLS
-  - GNUstep Web framework
-
-  This is because I don't need them now. 
-  If you want support for them investigate this site which has original script I referenced from.
-  [http://brilliantobjc.blogspot.kr/2012/12/cocoa-on-freebsd.html](http://brilliantobjc.blogspot.kr/2012/12/cocoa-on-freebsd.html)
+  If you want support for them investigate this [Johannes Lundberg's site](http://brilliantobjc.blogspot.kr/2012/12/cocoa-on-freebsd.html)
+  which has original script that I mostly copied from.
 
 - `objc2-centos6.sh`
 
   Experimental build script for CentOS 6.
+  
+  
+  
--- objc2-fb9.sh
+++ objc2-fb9.sh
@@ -1,13 +1,11 @@
 #!/bin/tcsh
 echo "Written for tcsh."
-echo "Run with /bin/tsch explicitly."
-echo "Soon to be changed to zsh (with set-k)."
-
 echo "This script constructs Objective-C 2.0 environment on FreeBSD 9.2"
 echo "Referenced from: http://brilliantobjc.blogspot.kr/2012/12/cocoa-on-freebsd.html"
 
  
 
+echo "On FreeBSD 9, Clang is not yet default compiler."
 echo "Switch to Clang. Ensure you're using 3.3 or above."
 setenv CC clang
 setenv CXX clang++
@@ -17,16 +15,15 @@ setenv CPP clang-cpp
 echo "Prerequisites."
 echo "On FreeBSD 9.2, portsnap cannot be executed in a script."
 echo "Do it yourself manually if you didn't."
-echo "I removed 'security/gnutls' package because I don't need it."
-echo "But it's required if you want TLS support in GNUstep..."
+pkg_add -r gnutls
 pkg_add -r portmaster
 cd /usr/ports
 portmaster --no-confirm -G textproc/libxslt devel/icu devel/libdispatch devel/libffi
  
  
- 
-echo "GNUstep(make:2.6.5, base:1.24.5) + libobjc2(1.7) on FreeBSD 9.2."
+echo "GNUstep(make:2.6.5, base:1.24.5 r37245) + libobjc2(1.7) on FreeBSD 9.2."
 echo "Versions are specified explicitly to prevent future break."
+echo "(GNUstep base r37245 has cricial bug fix, so should be included.)"
 echo "Now downloading sources from SVN. This will take pretty long to"
 echo "initiate, so please wait for a few minutes."
 cd ~/
@@ -35,31 +32,37 @@ cd t1
 pkg_add -r subversion gmake
 svn co http://svn.gna.org/svn/gnustep/libs/libobjc2/releases/1.7 libobjc2-1.7 &
 svn co http://svn.gna.org/svn/gnustep/tools/make/tags/make-2_6_5 make-2_6_5 &
-svn co http://svn.gna.org/svn/gnustep/libs/base/tags/base-1_24_5 base-1_24_5 &
+echo "svn co http://svn.gna.org/svn/gnustep/libs/base/tags/base-1_24_5 base-1_24_5 &"
+svn co http://svn.gna.org/svn/gnustep/libs/base/trunk@37245 base-1_24_5-r37245 &
 wait
- 
-cd make-2_6_5
-./configure --enable-objc-nonfragile-abi
-gmake install
-cd ..
- 
-cd libobjc2-1.7
-gmake install
-cd ..
- 
+
+echo "Build and install gnustep-make without libobjc2."
 cd make-2_6_5
 ./configure --enable-objc-nonfragile-abi
 gmake install
 cd ..
 
+echo "Build and install libobjc2."
+cd libobjc2-1.7
+gmake install
+cd ..
+
+echo "Rebuild and reinstall gnustep-make to use libobjc2."
+cd make-2_6_5
+./configure --enable-objc-nonfragile-abi
+gmake install
+cd ..
+
+
+
 echo "Now we need to set some path to GNUstep to use gnustep-make."
 echo "source /usr/GNUstep/System/Library/Makefiles/GNUstep.csh"
  
-cd base-1_24_5
+cd base-1_24_5-r37245
 echo "I don't know why this named in this way… but the switch"
 echo " --disable-unicodeconstants actually just force to use UTF-8."
 echo "So no not worry about encoding."
-./configure --enable-fake-main --disable-unicodeconstants --disable-tls
+./configure --enable-fake-main --disable-unicodeconstants
 gmake install
 cd ..
  
@@ -76,12 +79,7 @@ cd ..
  
  
  
- 
- 
-echo "Don't forget to add these lines to your login script to make sure"
-echo "to use Clang for all further builds."
- 
- 
+
  
  
 
